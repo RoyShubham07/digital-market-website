0d2de17dd58a64f7f6f3651d7822bdf804163bf3
Shubham <royshubham567@gmail.com>
2026-02-27 21:04:16 +0530

    Added responsive hamburger menu

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Thank you for contacting us! We will get back to you shortly.');
-});
-document.querySelectorAll('nav a').forEach(link => {
-    link.addEventListener('click', function(event) {
-        event.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
-    });
-});
 const toggle = document.querySelector(".menu-toggle");
 const navLinks = document.querySelector(".nav-links");
 
