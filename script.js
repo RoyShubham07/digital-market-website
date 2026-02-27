4ca672ae00411b6c8eb05df89f69d2545bce9bd4
Shubham <royshubham567@gmail.com>
2026-02-27 21:01:16 +0530

    Added responsive hamburger menu

--- script.js
+++ script.js
@@ -8,3 +8,9 @@ document.querySelectorAll('nav a').forEach(link => {
         document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
     });
 });
+const toggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+toggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
